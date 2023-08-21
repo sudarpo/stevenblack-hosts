fda518f36457e5bd5ef038aca903f1f3bc1fdf39
SChong <26452138+sudarpo@users.noreply.github.com>
2023-08-21 12:29:32 +0800

    Combined several hosts into separate blacklist.

--- task-scheduler-run.cmd
+++ task-scheduler-run.cmd
@@ -7,7 +7,8 @@ git pull sb master
 git push
 
 echo Generating hosts files
-python updateHostsFile.py --extensions porn fakenews gambling --auto --nounifiedhosts --nogendata --noupdate --compress --output sc-hosts
+copy /Y data\adaway.org\hosts+data\StevenBlack\hosts+data\someonewhocares.org\hosts+data\URLHaus\hosts+data\yoyo.org\hosts blacklist2
+python updateHostsFile.py --extensions porn fakenews gambling --auto --nounifiedhosts -x blacklist2 --nogendata --noupdate --compress --output sc-hosts
 
 echo Updating hosts file
 copy /y sc-hosts\hosts C:\Windows\System32\drivers\etc\hosts
