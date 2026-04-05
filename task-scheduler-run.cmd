f0479c929bf83c35727e758bbb8b69ba64f65074
SChong <26452138+sudarpo@users.noreply.github.com>
2026-04-05 11:36:54 +0800

    Updated cmd script.

--- task-scheduler-run.cmd
+++ task-scheduler-run.cmd
@@ -1,6 +1,8 @@
 
 cd "C:\_SC\codes\steven-black-hosts"
 
+call .\_venv\Scripts\activate
+
 echo Update...
 git pull
 git pull sb master
@@ -10,7 +12,7 @@ echo Generating hosts files
 copy /Y data\adaway.org\hosts+data\someonewhocares.org\hosts+data\URLHaus\hosts+data\yoyo.org\hosts+data\UncheckyAds\hosts blacklist2
 python updateHostsFile.py --extensions fakenews gambling --auto --nounifiedhosts -x blacklist2 --nogendata --noupdate --compress --output sc-hosts
 
-echo Updating hosts file
-copy /y sc-hosts\hosts C:\Windows\System32\drivers\etc\hosts
+echo Updating hosts file C:\Windows\System32\drivers\etc\hosts > _log-copy.log
+copy /y sc-hosts\hosts C:\Windows\System32\drivers\etc\hosts >> _log-copy.log
 
 ipconfig /flushdns
